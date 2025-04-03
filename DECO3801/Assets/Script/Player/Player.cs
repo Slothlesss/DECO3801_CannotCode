5199e06d929080255b197a6f8a643db8febe9357
mattbezett <89570633+mattbezett@users.noreply.github.com>
2025-04-03 14:59:38 +1000

    Added game over screen & health handler
    Health handler ensures player can only lose hearts every 3 seconds to reduce difficulty.

--- DECO3801/Assets/Script/Player/Player.cs
+++ DECO3801/Assets/Script/Player/Player.cs
@@ -3,36 +3,68 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    private int health = 3;
     [SerializeField] private GameObject heart1;
     [SerializeField] private GameObject heart2;
     [SerializeField] private GameObject heart3;
-    private float targetTime = 5.0f;
+    [SerializeField] private GameObject gameOverPanel;
+    private int health;
+    private float lostHealth = 3f;
+    private float timer = 0f;
+    private bool canLoseHealth = true;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void Start()
     {
+        health = 3;
+        gameOverPanel.SetActive(false);
+    }
 
-        if (other.CompareTag("Enemy"))
+    private void Update()
+    {
+        HandleCollisions();
+    }
+
+    private void HandleCollisions()
+    {
+        // Check if 3 seconds has past since the last time player collided with an obstacle.
+        timer += Time.deltaTime;
+        if (timer > lostHealth)
         {
-            if (health == 3)
-            {
-                heart3.SetActive(false);
-            }
-            else if (health == 2)
-            {
-                heart2.SetActive(false);
-            }
-            else if (health == 1)
-            {
-                heart1.SetActive(false);
-            }
-            startTimer();
-            health -= 1;
+            canLoseHealth = true;
+            timer = 0f;
         }
     }
 
-    private void startTimer()
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        // If player collides with an enemy object, player loses health.
+        if (other.CompareTag("Enemy"))
+        {
+            if (canLoseHealth)
+            {
+                if (health == 3)
+                {
+                    heart3.SetActive(false);
+                }
+                else if (health == 2)
+                {
+                    heart2.SetActive(false);
+                }
+                else if (health == 1)
+                {
+                    // If player has last all 3 hearts, game is over.
+                    heart1.SetActive(false);
+                    gameOver();
+                }
 
+                health -= 1;
+                canLoseHealth = false;
+            }
+        }
+    }
+
+    private void gameOver()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
     }
 }
