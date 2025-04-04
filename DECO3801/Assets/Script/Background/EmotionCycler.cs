9e156dd177add2188056022edd480175f5004fcb
Ben <benmurphy9@hotmail.com>
2025-04-04 11:51:36 +1000

    Emotion levels link to pop-ups

--- DECO3801/Assets/Script/Background/EmotionCycler.cs
+++ DECO3801/Assets/Script/Background/EmotionCycler.cs
@@ -8,13 +8,14 @@ public class EmotionCycler : MonoBehaviour
     [SerializeField] private Image[] frustrationImages;
     [SerializeField] private Image[] focusImages;
     [SerializeField] private Image[] fatigueImages;
-
+    [SerializeField] private NotificationManager notificationManager;
     private int frustrationIndex = 0;
     private int focusIndex = 0;
     private int fatigueIndex = 0;
 
     private void Start()
     {
+        notificationManager = FindObjectOfType<NotificationManager>();
         SetEmotionColor(frustrationImages, frustrationIndex);
         SetEmotionColor(focusImages, focusIndex);
         SetEmotionColor(fatigueImages, fatigueIndex);
@@ -29,17 +30,35 @@ public class EmotionCycler : MonoBehaviour
         }
     }
 
+    // Get emotion indexes.
+    public int GetFrustrationIndex()
+    {
+        return frustrationIndex;
+    }
+
+    public int GetFocusIndex()
+    {
+        return focusIndex;
+    }
+
+    public int GetFatigueIndex()
+    {
+        return fatigueIndex;
+    }
+    
     // === Frustration ===
     public void FrustrationCycleUp()
     {
         frustrationIndex = (frustrationIndex + 1) % frustrationImages.Length;
         SetEmotionColor(frustrationImages, frustrationIndex);
+        notificationManager.FrustrationHandler(frustrationIndex);
     }
 
     public void FrustrationCycleDown()
     {
         frustrationIndex = (frustrationIndex - 1 + frustrationImages.Length) % frustrationImages.Length;
         SetEmotionColor(frustrationImages, frustrationIndex);
+        notificationManager.FrustrationHandler(frustrationIndex);
     }
 
     // === Focus ===
@@ -47,12 +66,14 @@ public class EmotionCycler : MonoBehaviour
     {
         focusIndex = (focusIndex + 1) % focusImages.Length;
         SetEmotionColor(focusImages, focusIndex);
+        notificationManager.FocusHandler(focusIndex);
     }
 
     public void FocusCycleDown()
     {
         focusIndex = (focusIndex - 1 + focusImages.Length) % focusImages.Length;
         SetEmotionColor(focusImages, focusIndex);
+        notificationManager.FocusHandler(focusIndex);
     }
 
     // === Fatigue ===
@@ -60,11 +81,13 @@ public class EmotionCycler : MonoBehaviour
     {
         fatigueIndex = (fatigueIndex + 1) % fatigueImages.Length;
         SetEmotionColor(fatigueImages, fatigueIndex);
+        notificationManager.FatigueHandler(fatigueIndex);
     }
 
     public void FatigueCycleDown()
     {
         fatigueIndex = (fatigueIndex - 1 + fatigueImages.Length) % fatigueImages.Length;
         SetEmotionColor(fatigueImages, fatigueIndex);
+        notificationManager.FatigueHandler(fatigueIndex);
     }
 }
--- DECO3801/Assets/Script/Background/NotificationManager.cs
+++ DECO3801/Assets/Script/Background/NotificationManager.cs
@@ -10,9 +10,11 @@ public class NotificationManager : MonoBehaviour
 [SerializeField] private TextMeshProUGUI secondTitleUI;
 [SerializeField] private TextMeshProUGUI secondContentUI;
     private GameObject currentPopUp;
+    private EmotionCycler emotionCycler;
     private void Start() {
         PopUp("FirstNotification", false);
         PopUp("SecondNotification", false);
+
     }
     public void PopUp(string name, bool isActive)
     {
@@ -62,4 +64,61 @@ public class NotificationManager : MonoBehaviour
         secondContentUI.text = content;
     }
 
+    // Show notification based on the current state of notifications.
+    // If first is active, update second. If second is active, update first.
+   private void ShowNotification(string title, string content, bool useFirstNotification)
+    {
+        if (useFirstNotification)
+        {
+            UpdateFirstNotification(title, content);
+            PopUp("FirstNotification", true);
+        }
+        else
+        {
+            UpdateSecondNotification(title, content);
+            PopUp("SecondNotification", true);
+        }
+    }
+
+    public void FrustrationHandler(int frustrationIndex)
+    {
+        string title = "";
+        string content = "";
+        switch (frustrationIndex)
+        {
+            case 0: title = "You are calm."; content = "You are doing well!"; break;
+            case 1: title = "You are slightly frustrated."; content = "Take a break if you need to."; break;
+            case 2: title = "You are moderately frustrated."; content = "Consider taking a break."; break;
+            case 3: title = "You are seriously frustrated."; content = "Take a break now!"; break;
+        }
+        ShowNotification(title, content, true);
+    }
+
+    public void FocusHandler(int focusIndex)
+    {
+        string title = "";
+        string content = "";
+        switch (focusIndex)
+        {
+            case 0: title = "You are adequately focused."; content = "You are doing well!"; break;
+            case 1: title = "You are slightly distracted."; content = "Try to refocus."; break;
+            case 2: title = "You are moderately distracted."; content = "Consider taking a break."; break;
+            case 3: title = "You are seriously distracted."; content = "Take a break now!"; break;
+        }
+        ShowNotification(title, content, false);
+    }
+
+    public void FatigueHandler(int fatigueIndex)
+    {
+        string title = "";
+        string content = "";
+        switch (fatigueIndex)
+        {
+            case 0: title = "You are adequately rested."; content = "You are doing well!"; break;
+            case 1: title = "You are slightly fatigued."; content = "Take a break if you need to."; break;
+            case 2: title = "You are moderately fatigued."; content = "Consider taking a break."; break;
+            case 3: title = "You are seriously fatigued."; content = "Take a break now!"; break;
+        }
+        ShowNotification(title, content, false);
+    }
 }
